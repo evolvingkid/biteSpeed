be9ba05fe1d32c392a4dfeebc9cae2082a36e6ce
mohammed fayez <fayezmohammed23@gmail.com>
2024-03-07 02:08:22 +0530

    feat: Initail setup

---
+++ src/components/nav/index.jsx
@@ -0,0 +1,9 @@
+import classes from './style.module.css'
+
+const Nav = () => {
+  return <nav className={`${classes?.nav} pt-1 `}>
+
+  </nav>;
+};
+
+export default Nav;
---
+++ src/page/home/index.jsx
@@ -0,0 +1,11 @@
+import Nav from "../../components/nav";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+};
+
+export default HomePage;
